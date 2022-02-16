ec3a1af40a7a3c65622a7013152edd4bb045b1cc
Theunis Knoetze <theunis.knoetze@gmail.com>
2022-02-16 17:55:26 +0300

    credentials complete

--- src/router.js
+++ src/router.js
@@ -13,12 +13,31 @@ const mockUser = {
     }
 };
 
+const secret = "doesntMatter";
+
 router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === mockUser.username || password === mockUser.password) {
+        
+        const token = jwt.sign(mockUser, secret)
+        res.json({ accessToken: token });
+
+    } else {
+        res.send("The username or password is incorrect.");
+    }
 
 });
 
 router.get('/profile', (req, res) => {
+    const userToken = req.get("Authorization");
 
+    try {
+        const decoded = jwt.verify(userToken, secret);
+        res.json(decoded.profile);
+    } catch(err) {
+        res.send(err)
+    }
+    
 });
 
 module.exports = router;
